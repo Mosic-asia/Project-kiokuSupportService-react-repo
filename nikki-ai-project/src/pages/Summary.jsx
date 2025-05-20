47de752a035aded948145fec2086402dc78f6fc7
Bacococc <pphj7353@naver.com>
2025-05-20 18:42:37 +0900

    📝 Add Pages
    Add pages

---
+++ nikki-ai-project/src/pages/Summary.jsx
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom';
+
+function Summary() {
+  return (
+    <div>
+      <h1>서머리 화면</h1>
+      <Link to="/">홈으로 돌아가기</Link>
+    </div>
+  );
+}
+
+export default Summary;
