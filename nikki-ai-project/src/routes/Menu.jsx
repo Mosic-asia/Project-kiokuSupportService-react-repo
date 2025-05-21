61ec9b4fe228abbef8a476fefe6327aea3d1e8bb
seulzutora <seulzutora@gmail.com>
2025-05-21 20:18:05 +0900

    💄 Add menu style
    added menu stylessss

--- nikki-ai-project/src/routes/Menu.jsx
+++ nikki-ai-project/src/routes/Menu.jsx
@@ -1,4 +1,37 @@
-function Menu() {
+import React from 'react';
+import '../styles/Menu.css';
 
-}
-export default Menu 
+const Menu = () => {
+    return (
+        <div className="menu-container">
+            {/* 緊急連絡先 */}
+            <button className="emergency-contact-button">
+                緊急連絡先
+            </button>
+
+            {/* 메뉴 아이템 */}
+            <div className="menu-grid">
+                <div className="menu-item">
+                    <span className="item-text">日記</span>
+                </div>
+                <div className="menu-item">
+                    <span className="item-text">日記を書く</span>
+                </div>
+                <div className="menu-item">
+                    <span className="item-text">トーク</span>
+                </div>
+                <div className="menu-item">
+                    <span className="item-text">予定</span>
+                </div>
+                <div className="menu-item">
+                    <span className="item-text">マイカルて</span>
+                </div>
+                <div className="menu-item">
+                    <span className="item-text">設定</span>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Menu;
