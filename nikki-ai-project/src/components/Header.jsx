b194d07a12e542db51e8c7f4ef3f946cb558e2b7
Bacococc <pphj7353@naver.com>
2025-05-21 23:03:23 +0900

    💄 Update chat UI
    updated chat ui

--- nikki-ai-project/src/components/Header.jsx
+++ nikki-ai-project/src/components/Header.jsx
@@ -2,14 +2,16 @@
 import React from 'react';
 import styles from '../styles/Header.module.css';
 import HomeIcon from '../assets/home.svg'; // 홈 아이콘 경로 확인 및 수정
+import { Link } from 'react-router-dom';
+
 
 const Header = () => {
   return (
     <header className={styles.header}>
       <div className={styles.title}>暖かい日記</div>
-      <div className={styles.homeButton}>
-        <img src={HomeIcon} alt="Home" className={styles.homeIcon} />
-      </div>
+      <Link to="/menu">
+          <img src={HomeIcon} alt="Home" className={styles.homeIcon} />
+        </Link>
     </header>
   );
 };
